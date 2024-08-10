9abbdf4fdd6fe8bd4cb2bd38442d9912234af56a
Ahmet Burak Arslan <arslanahmet4772@gmail.com>
2024-08-10 23:38:44 +0200

    Design foundations were created

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
-import { useState, useEffect } from 'react';
 import axios from 'axios';
+import weatherCode from './lib/weathercode.json';
+import { useState, useEffect } from 'react';
+import { List, Search } from 'lucide-react';
 
 async function getWeather() {
   const userIp: string = await axios.get("https://api.ipify.org?format=json")
@@ -15,8 +17,9 @@ async function getWeather() {
       };
     });
 
-  const weatherData: any = await axios.get(`https://api.open-meteo.com/v1/forecast?latitude=${locationData.lat}&longitude=${locationData.lon}&hourly=temperature_2m`)
+  const weatherData: any = await axios.get(`https://api.open-meteo.com/v1/forecast?latitude=${locationData.lat}&longitude=${locationData.lon}&hourly=temperature_2m,weather_code`)
     .then((response) => {
+      console.log(response.data);
       return response.data;
     });
 
@@ -24,7 +27,8 @@ async function getWeather() {
 
   return {
     temperature: weatherData.hourly.temperature_2m.slice(time, time + 12),
-    time: weatherData.hourly.time.slice(time, time + 12)
+    time: weatherData.hourly.time.slice(time, time + 12),
+    weatherCode: weatherData.hourly.weather_code
   };
 }
 
@@ -47,14 +51,22 @@ function App() {
   }, []);
 
   return (
-    <div>
-      <h1>Hava Durumu</h1>
+    <div className='flex flex-col w-screen h-screen bg-zinc-900 items-center justify-center'>
+      <div className='flex flex-row bg-zinc-800 p-8 h-24 w-[800px] rounded-3xl shadow-2xl'>
+        <h1 className='text-white text-3xl font-bold'>Weather</h1>
+        <h1 className='text-zinc-500 text-xl mt-3 ml-4'>{new Date().toLocaleDateString()}</h1>
+        <button className='flex items-center justify-center ml-96 w-9 h-9 p-2 rounded-md text-white hover:border-2'><Search /></button>
+        <button className='flex items-center justify-center ml-4 w-9 h-9 p-2 rounded-md text-white hover:border-2'><List /></button>
+      </div>
       <div>
-        {time.map((t, index) => (
-          <div key={index}>
-            <span>{t}</span>: <span>{temperature[index]}°C</span>
-          </div>
-        ))}
+        {temperature && time !== null ?
+          (
+            <div className='w-[800px] h-96 mt-4 bg-zinc-800 p-8 rounded-3xl shadow-2xl overflow-y-auto '>
+              <img src={weatherCode["0"].icon} alt="" />
+            </div>
+          ) : (
+            <div className='w-[800px] h-24 mt-4 bg-zinc-800 p-8 rounded-3xl shadow-2xl'><span className='text-white text-xl'>No weather data found</span></div>
+          )}
       </div>
     </div>
   );
